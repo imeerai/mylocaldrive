c634dbed0c6cd14921cbbe21febebab5aeab45c5
imeer.ai <abbaszameer234@gmail.com>
2026-02-28 14:57:23 +0500

    config/r2.js: chnge define

--- config/r2.js
+++ config/r2.js
@@ -1,4 +1,5 @@
 const { S3Client } = require('@aws-sdk/client-s3');
+const { NodeHttpHandler } = require('@smithy/node-http-handler');
 
 const required = ['R2_ENDPOINT', 'R2_ACCESS_KEY_ID', 'R2_SECRET_ACCESS_KEY'];
 const missing = required.filter((name) => !process.env[name]);
@@ -10,6 +11,11 @@ if (missing.length) {
 const client = new S3Client({
   region: 'auto',
   endpoint: process.env.R2_ENDPOINT,
+  maxAttempts: Number(process.env.R2_MAX_ATTEMPTS || 2),
+  requestHandler: new NodeHttpHandler({
+    connectionTimeout: Number(process.env.R2_CONNECTION_TIMEOUT_MS || 10000),
+    socketTimeout: Number(process.env.R2_SOCKET_TIMEOUT_MS || 120000),
+  }),
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
